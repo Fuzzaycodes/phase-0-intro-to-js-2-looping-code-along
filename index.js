7a2147eafad82d59828f7dafa8280c422f1078d9
Fuzzay <samomwerif2@gmail.com>
2024-07-09 12:41:05 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function countDown(startingNumber) {
+    let count = startingNumber;
+  
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  
+
+function writeCards(names, event) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  
